fe24803825e375ee5d0f170ff80cab43a1296f91
samiya-k <samiya@hicounselor.com>
2023-12-08 17:35:38 +0530

    Notes single page added

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.list)
+    path('list', views.list),
+    path('<int:pk>/', views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import Http404
 # Create your views here.
 def list(request):
     notes = Notes.objects.all()
     return render(request, 'notes/note_list.html', {'notes': notes})
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404('Note does not exist')
+    return render(request, 'notes/note_detail.html', {'note': note})
