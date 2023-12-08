912aa7f8d06871765aae84131d87d690c0481cae
samiya-k <samiya@hicounselor.com>
2023-12-09 00:04:36 +0530

    Sign Up Page Added

--- home/views.py
+++ home/views.py
@@ -3,6 +3,19 @@ from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+class SignupView(TemplateView):
+    form_class= UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/notes/list'
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        return render(request, self.template_name, {'form' : form})
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
