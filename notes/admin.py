0cb2f5dd4667f1b1f90a8743b65b22b9aefae6b5
samiya-k <samiya@hicounselor.com>
2023-12-08 10:19:33 +0530

    NotesAdmin Class created | Now we can add notes from backend or admin page

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Notes
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+
+admin.site.register(Notes, NotesAdmin)
+    
