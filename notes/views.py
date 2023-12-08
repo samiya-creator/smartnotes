21df68773f4e167d7c1bd502f7b6beeb5c8a3c40
samiya-k <samiya@hicounselor.com>
2023-12-08 23:38:47 +0530

    Foregin key added in DB

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Notes
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import NotesForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class NotesCreateView(CreateView):
     model =Notes
     form_class = NotesForm
     success_url = '/notes/list'
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
     
 class NotesUpdateView(UpdateView):
     model = Notes
@@ -19,9 +27,13 @@ class NotesDeleteView(DeleteView):
     success_url = '/notes/list'
     template_name = 'notes/notes_delete.html'
     
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = 'notes'
+    login_url = '/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
 class NotesDetailView(DetailView):
     model = Notes
