f250da78aa767cf76fe1b686246123c7b9cb014d
samiya-k <samiya@hicounselor.com>
2023-12-08 18:07:25 +0530

    Class View Templates Explored

--- home/views.py
+++ home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    # return HttpResponse("Hello, Django!")
-    return render(request, 'home/welcome.html', {'name': 'Django', 'current_time': datetime.now()})
-
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'name': 'Django', 'current_time': datetime.now()}
+    
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView, DetailView
 # Create your views here.
-def list(request):
-    notes = Notes.objects.all()
-    return render(request, 'notes/note_list.html', {'notes': notes})
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404('Note does not exist')
-    return render(request, 'notes/note_detail.html', {'note': note})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = 'notes'
+    
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
