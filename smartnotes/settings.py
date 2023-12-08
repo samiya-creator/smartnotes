c6cf1f1b62318a9db4f127a9d4f8c9b80e3843d3
samiya-k <samiya@hicounselor.com>
2023-12-08 21:52:29 +0530

    Base html created | common template

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'smartnotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'static/templates',
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
